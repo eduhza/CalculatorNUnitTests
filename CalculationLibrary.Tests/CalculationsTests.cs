6b4b1eee5bd81a237ba1eeaf02170c67937ea092
Eduardo Arruda <eduhza@gmail.com>
2023-04-16 16:31:54 -0300

    Add project files.

---
+++ CalculationLibrary.Tests/CalculationsTests.cs
@@ -0,0 +1,75 @@
+﻿namespace CalculationLibrary.Tests
+{
+    public class CalculationsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        [Theory]
+        [TestCase(4, 2, 6)]
+        [TestCase(0, 0, 0)]
+        [TestCase(1234, -1234, 0)]
+        public void AddShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculations calculator = new Calculations();
+
+            //Act
+            double actual = calculator.Add(x, y);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Theory]
+        [TestCase(4, 2, 2)]
+        [TestCase(0, 0, 0)]
+        [TestCase(1234, -1234, 2468)]
+        public void SubtractShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculations calculator = new Calculations();
+
+            //Act
+            double actual = calculator.Subtract(x, y);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Theory]
+        [TestCase(4, 2, 8)]
+        [TestCase(0, 0, 0)]
+        [TestCase(1234, -1234, -1522756)]
+        public void MultiplyShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculations calculator = new Calculations();
+
+            //Act
+            double actual = calculator.Multiply(x, y);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Theory]
+        [TestCase(4, 2, 2)]
+        [TestCase(0, 0, 0)]
+        [TestCase(1234, -1234, -1)]
+        public void DivideShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculations calculator = new Calculations();
+
+            //Act
+            double actual = calculator.Divide(x, y);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Calculator/Calculations.cs
@@ -0,0 +1,37 @@
+﻿
+namespace CalculationLibrary {
+    public class Calculations {
+
+        public double Add(double x, double y)
+        {
+            //throw new NotImplementedException("Please create a test first.");
+            return x + y;
+        }
+        public double Subtract(double x, double y)
+        {
+            //throw new NotImplementedException("Please create a test first.");
+            return x - y;
+
+        }
+        public double Multiply(double x, double y)
+        {
+            //throw new NotImplementedException("Please create a test first.");
+            return x * y;
+
+        }
+        public double Divide(double x, double y)
+        {
+            //throw new NotImplementedException("Please create a test first.");
+            //return x / y;
+
+            // Refactor
+            if(y != 0)
+            {
+                return x / y;
+            }
+
+            return 0;
+
+        }
+    }
+}
